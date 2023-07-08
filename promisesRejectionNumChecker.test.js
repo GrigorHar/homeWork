51c229207e8a4e3e39d6ad7ee0dba37b9f4c7da4
Grigor Harutyunyan <48802980+GrigorHar@users.noreply.github.com>
2023-07-08 19:31:25 +0400

    Update promisesRejectionNumChecker.test.js

--- promisesRejectionNumChecker.test.js
+++ promisesRejectionNumChecker.test.js
@@ -1,38 +1,66 @@
-const { promisesRejectionNumChecker } = require('./promisesRejectionNumChecker');
+ const { myPromise } = require('./promisesRejectionNumChecker');
 
-describe('promisesRejectionNumChecker', () => {
-    test('should resolve if no promises are provided', async () => {
-        const promises = [];
-        const rejectionThreshold = 2;
-    
-        await expect(promisesRejectionNumChecker(promises, rejectionThreshold)).resolves.toBeUndefined();
-      });
-    
-      test('should resolve if rejectionThreshold is greater than the number of rejected promises', async () => {
-        const promises = [Promise.reject(), Promise.reject(), Promise.reject()];
-        const rejectionThreshold = 5;
-    
-        await expect(promisesRejectionNumChecker(promises, rejectionThreshold)).resolves.toBeUndefined();
-      });
-    
-      test('should resolve if some promises are rejected but not exceeding the threshold', async () => {
-        const promises = [Promise.resolve(), Promise.reject(), Promise.resolve()];
-        const rejectionThreshold = 2;
-    
-        await expect(promisesRejectionNumChecker(promises, rejectionThreshold)).resolves.toBeUndefined();
-      });
-    
-      test('should reject if a single promise is rejected and exceeds the threshold', async () => {
-        const promises = [Promise.reject()];
-        const rejectionThreshold = 0;
-    
-        await expect(promisesRejectionNumChecker(promises, rejectionThreshold)).rejects.toThrowError(
-          'Number of rejected promises exceeded the threshold of 0'
-        );
-      });
+describe('myPromise', () => {
 
+  it('should resolve with "resolved" if the number of rejected promises is not greater than the specified number', async () => {
+    const promises = [
+      Promise.resolve(1),
+      Promise.resolve(2),
+      Promise.reject(),
+    ];
 
+    const result = await myPromise(promises, 1);
+    expect(result).toBe('resolved');
+  });
 
+  it('should reject with an error if the number of rejected promises is greater than the specified number', async () => {
+    const promises = [
+      Promise.resolve(1),
+      Promise.reject(),
+      Promise.reject(),
+    ];
+
+    await expect(myPromise(promises, 1)).rejects.toThrowError('Too many promises rejected');
+  });
+
+  it('should resolve with "resolved" if the number of rejected promises is zero', async () => {
+    const promises = [
+      Promise.resolve(1),
+      Promise.resolve(2),
+    ];
+
+    const result = await myPromise(promises, 0);
+    expect(result).toBe('resolved');
+  });
+
+  it('should resolve with "resolved" if the number of rejected promises is equal to the specified number', async () => {
+    const promises = [
+      Promise.resolve(1),
+      Promise.reject(),
+    ];
+
+    const result = await myPromise(promises, 1);
+    expect(result).toBe('resolved');
+  });
+
+  it('should resolve with "resolved" if the number of rejected promises is less than the specified number', async () => {
+    const promises = [
+      Promise.resolve(1),
+      Promise.resolve(2),
+      Promise.reject(),
+    ];
+
+    const result = await myPromise(promises, 2);
+    expect(result).toBe('resolved');
+  });
+
+  it('should reject with an error if all promises are rejected', async () => {
+    const promises = [
+      Promise.reject(),
+      Promise.reject(),
+      Promise.reject(),
+    ];
   
-  
+    await expect(myPromise(promises, 2)).rejects.toThrowError('Too many promises rejected');
+  });
 });
